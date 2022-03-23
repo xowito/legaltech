36dc592cb470d871f63347718123395ef7ff8c0b
xowito <xowito01@agmail.com>
2022-03-23 01:58:12 -0300

    Ver detalle demanda mas un par de cosas v2

--- lt_env/legaltech_site/legaltech_app/urls.py
+++ lt_env/legaltech_site/legaltech_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home,demandas 
+from .views import home,demandas,nueva_demanda, detalle_demanda
 
 
 
 urlpatterns = [
     path('', home, name='home'),
-    path('demandas',demandas,name='demandas')
+    path('nueva_demanda', nueva_demanda, name='nueva_demanda'),
+    path('demandas',demandas,name='demandas'),
+    path('detalle_demanda/<id>/',detalle_demanda,name='detalle_demanda')
 ]
--- lt_env/legaltech_site/legaltech_app/views.py
+++ lt_env/legaltech_site/legaltech_app/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
 from .models import Demanda
 from .forms import demanda_form
 # Create your views here.
 def home(request):
-    formulario = demanda_form(request.POST)
+    return render(request,'legaltech_app/home.html')
+
+
+def nueva_demanda(request):
+    formulario = demanda_form()
     data={"form":formulario}
+    
+    if request.method == 'POST':
+        formulario = demanda_form(data=request.POST or None)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect(to="home")
     return render (request, 'legaltech_app/nueva_demanda.html',data)
 
 def demandas(request):
     demanda = Demanda.objects.all()
     data = {"obj":demanda}
     return render(request,'legaltech_app/demandas.html',data)
+
+def detalle_demanda(request,id):
+    
+    detalle = Demanda.objects.get(Id=id)
+    data = {"obj":detalle}
+    return render(request,'legaltech_app/detalle_demanda.html',data)
+    
+
+    
